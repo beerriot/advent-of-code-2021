f52e178b0ddcca807fc34d1c015f88523600eaa5
Bryan Fink <bryan.fink@gmail.com>
2021-12-08 10:09:03 -0600

    Puzzle 6B

--- src/puzzle06.erl
+++ src/puzzle06.erl
@@ -6,22 +6,52 @@
 
 -export([
          solveA/0,
-         model_fish/2
+         solveB/0,
+         model_fishA/2,
+         model_fishB/2,
+         model_fishB/3
         ]).
 
 solveA() ->
+    model_fishA(80, load_fish()).
+
+solveB() ->
+    model_fishB(256, load_fish()).
+
+load_fish() ->
     {ok, Data} = file:read_file("puzzles/puzzle06-input.txt"),
     [OneLine|_] = string:split(Data, <<"\n">>),
-    Fish = [ binary_to_integer(F) ||
-               F <- string:split(OneLine, <<",">>, all) ],
-    model_fish(80, Fish).
+    [ binary_to_integer(F) ||
+        F <- string:split(OneLine, <<",">>, all) ].
 
-model_fish(0, Fish) ->
+model_fishA(0, Fish) ->
     Fish;
-model_fish(Days, Fish) ->
-    model_fish(
+model_fishA(Days, Fish) ->
+    model_fishA(
       Days-1,
       lists:foldl(fun(0, Acc) -> [8, 6 | Acc];
                      (F, Acc) -> [F-1 | Acc]
                   end,
                   [], Fish)).
+
+model_fishB(Days, Fish) ->
+    BDays = [ F-6 || F <- Fish ],
+    Table = model_fishB(Days, BDays, []),
+    lists:sum([ proplists:get_value(F, Table) || F <- BDays ]).
+
+model_fishB(_, [], Table) ->
+    Table;
+model_fishB(Days, [Fish|Rest], Table) ->
+    case proplists:is_defined(Fish, Table) of
+        true ->
+            model_fishB(Days, Rest, Table);
+        false when Fish >= Days ->
+            model_fishB(Days, Rest, [{Fish, 1} | Table]);
+        false ->
+            Spawn = [ F + 2 || %% first spawn delay
+                        F <- lists:seq(Fish+7, Days, 7) ],
+            NewTable = model_fishB(Days, Spawn, Table),
+            Total = lists:sum([ proplists:get_value(S, NewTable) ||
+                                  S <- Spawn ]),
+            model_fishB(Days, Rest, [{Fish, Total+1} | NewTable])
+    end.
