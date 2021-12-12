6b21fc04301ceff4c22f0aa527abd5859da25591
Bryan Fink <bryan.fink@gmail.com>
2021-12-12 00:09:32 -0600

    Puzzle 12B

--- src/puzzle12.erl
+++ src/puzzle12.erl
@@ -5,13 +5,15 @@
 -module(puzzle12).
 
 -export([
-         solveA/0
+         solveA/0,
+         solveB/0
         ]).
-
--compile([export_all]).
 
 solveA() ->
     count_paths(<<"start">>, load_network()).
+
+solveB() ->
+    count_paths(<<"start">>, load_network(), []).
 
 load_network() ->
     {ok, Data} = file:read_file("puzzles/puzzle12-input.txt"),
@@ -46,6 +48,43 @@ count_paths(Start, Network) ->
             lists:sum([ case L of
                             <<"end">> -> 1;
                             _ -> count_paths(L, RealRest)
+                        end || L <- InitialLinks ]);
+        false ->
+            0
+    end.
+
+count_paths(Start, Network, Visited) ->
+    case lists:keytake(Start, 1, Network) of
+        {value, {Start, InitialLinks}, Rest} ->
+            {RealRest, NewVisited} =
+                case Start of
+                    <<C, _/binary>> when C >= $A, C =< $Z ->
+                        {[{Start, InitialLinks}|Rest], Visited};
+                    <<"start">> ->
+                        {Rest, Visited};
+                    _ ->
+                        case Visited of
+                            complete ->
+                                {Rest, complete};
+                            _ ->
+                                case lists:member(Start, Visited) of
+                                    true ->
+                                        {lists:foldl(
+                                           fun(V, Acc) ->
+                                                   lists:keydelete(V, 1, Acc)
+                                           end,
+                                           Rest,
+                                           Visited),
+                                         complete};
+                                    false ->
+                                        {[{Start, InitialLinks}|Rest],
+                                         [Start|Visited]}
+                                end
+                        end
+                end,
+            lists:sum([ case L of
+                            <<"end">> -> 1;
+                            _ -> count_paths(L, RealRest, NewVisited)
                         end || L <- InitialLinks ]);
         false ->
             0
