f960d04fc7e688f879cc9be394c04304d9f45de8
Bryan Fink <bryan.fink@gmail.com>
2021-12-13 10:21:32 -0600

    Puzzle 13B

--- src/puzzle13.erl
+++ src/puzzle13.erl
@@ -7,13 +7,20 @@
 
 -export([
          solveA/0,
+         solveB/0,
          parse_input/1,
-         fold_paper/2
+         fold_paper/2,
+         render_dots/1
         ]).
 
 solveA() ->
     {Dots, [FirstFold|_]} = load_file(),
     fold_paper(Dots, [FirstFold]).
+
+solveB() ->
+    {Dots, Folds} = load_file(),
+    FinalDots = fold_paper(Dots, Folds),
+    render_dots(FinalDots).
 
 load_file() ->
     {ok, Data} = file:read_file("puzzles/puzzle13-input.txt"),
@@ -49,3 +56,19 @@ fold_paper(Dots, [{Axis,Value}|Rest]) ->
                          element(Axis, D) - 2 * (element(Axis, D) - Value))
               || D <- BelowRight ],
     fold_paper(ordsets:union(AboveLeft, ordsets:from_list(Moved)), Rest).
+
+render_dots(Dots) ->
+    Width = 1 + lists:max([ X || {X,_} <- Dots ]),
+    Height = 1 + lists:max([ Y || {_, Y} <- Dots ]),
+    Image = lists:duplicate(Height, lists:duplicate(Width, $.)),
+    render_dots(Dots, Image).
+
+render_dots([], Image) ->
+    lists:foreach(fun(L) -> io:format("~s~n", [L]) end, Image),
+    Image;
+render_dots([{X,Y}|Rest], Image) ->
+    {RowsBefore, [Row|RowsAfter]} = lists:split(Y, Image),
+    {ColumnsBefore, [_|ColumnsAfter]} = lists:split(X, Row),
+    render_dots(Rest,
+                RowsBefore ++ [(ColumnsBefore ++ [$#|ColumnsAfter])
+                               |RowsAfter]).
